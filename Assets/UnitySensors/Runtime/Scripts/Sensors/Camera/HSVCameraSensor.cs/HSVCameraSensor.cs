741765902c0c9a98c49e6f01f00a74c0f054cf8b
Jared <j.a.lindsay06@gmail.com>
2024-07-15 19:01:04 +1000

    hsv camera sensor initial

---
+++ Assets/UnitySensors/Runtime/Scripts/Sensors/Camera/HSVCameraSensor.cs/HSVCameraSensor.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnitySensors.Data.Texture;
+
+namespace UnitySensors.Sensor.Camera {
+    public class HSVCameraSensor : CameraSensor {
+
+        [SerializeField] private float hueMin;
+        [SerializeField] private float hueMax;
+        [SerializeField] private float saturationMin;
+        [SerializeField] private float saturationMax;
+        [SerializeField] private float valueMin;
+        [SerializeField] private float valueMax;
+
+        private Texture2D filteredImage;
+
+        private Color b = new Color(0, 0, 0, 0);
+
+        protected override void Init()
+        {
+            base.Init();
+
+            filteredImage = new Texture2D(resolution.x, resolution.y, TextureFormat.Alpha8, false);
+        }
+
+        private void UpdateSensor()
+        {
+            // Get the RGB image from the unity camera
+
+            Texture2D rgbImage = texture;
+
+            // Filter the image based on HSV values
+            for (int y = 0; y < rgbImage.height; y++)
+            {
+                for (int x = 0; x < rgbImage.width; x++)
+                {
+                    Color pixelColor = rgbImage.GetPixel(x, y);
+                    float h;
+                    float s;
+                    float v;
+                    Color.RGBToHSV(pixelColor, out h, out s, out v);
+
+                    // Check if the pixel falls within the specified HSV range
+                    if (h >= hueMin && h <= hueMax &&
+                        s >= saturationMin && s <= saturationMax &&
+                        v >= valueMin && v <= valueMax)
+                    {
+                        filteredImage.SetPixel(x, y, Color.clear);
+                    }
+                    else
+                    {
+                        filteredImage.SetPixel(x, y, b);
+                    }
+                }
+            }
+
+            // Apply the changes to the filtered image texture
+            filteredImage.Apply();
+        }
+
+        public Texture2D GetFilteredImage()
+        {
+            return filteredImage;
+        }
+    }
+}
